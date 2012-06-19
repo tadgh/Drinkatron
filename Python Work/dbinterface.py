545b6760c56594ff85903a615ad0ca84273289c1
Gary Graham <garygrantgraham@gmail.com>
2012-06-18 20:34:34 -0400

    Initial commit of the database interface. Just a placeholder for now

---
+++ Python Work/dbinterface.py
@@ -0,0 +1,17 @@
+import re
+import sqlite3
+
+
+class DB:
+    def __init__:
+        conn = sqlite3.connect('TEMPFILE')
+        hook = conn.cursor()
+
+    def listDrinksByName(self):
+        pass
+
+    def updateDrinkScore(self, scoreModifier):
+        pass
+
+    def listDrinksByIngredient(self, ingredient):
+        pass
