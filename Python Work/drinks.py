346daaf169b304f5b3c6c94f184baf89ccdd0218
Gary Graham <garygrantgraham@gmail.com>
2012-06-21 15:23:17 -0400

    Added GenerateList and DispenseTime functions[#3]
    This will allow for cleaner passing of bytes through arduinocomm to the arduino

--- Python Work/drinks.py
+++ Python Work/drinks.py
@@ -22,6 +22,7 @@ class drink:
         self.id = 0
         self.drinkName = ""
         self.cost = 0.00
+        self.ingredientListCleaned = []
 
 
     def dispenseDrinkInParallel(self):
@@ -41,4 +42,12 @@ class drink:
 
     def popularityIncrement(self):
         self.popularity += 1
+
+    def getDispenseTime(self):
+        ingredientList =  [ing1, ing2, ing3, ing4, ing5, ing6, ing7, ing8, ing9, ing10, ing11, ing12]
+        return max(ingredientList)
+
+    def generateListForArduino(self):
+        self.ingredientListCleaned = [ing1, ing2, ing3, ing4, ing5, ing6, ing7, ing8, ing9, ing10, ing11, ing12]
+        return self.ingredientListCleaned
 
