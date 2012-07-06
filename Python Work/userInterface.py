ecbb787304df5ccab1d748f55d110bde8161d85c
Gary Graham <garygrantgraham@gmail.com>
2012-07-05 21:54:49 -0400

    Added a couple of buttons, just working on GUI stuff #5

--- Python Work/userInterface.py
+++ Python Work/userInterface.py
@@ -6,7 +6,19 @@ import logging
 
 
 
+class StatusBar(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.label = Label(self, bd=1, relief=SUNKEN, anchor=W)
+        self.label.pack(fill=X)
 
+    def set(self, format, *args):
+        self.label.config(text=format % args)
+        self.label.update_idletasks()
+
+    def clear(self):
+        self.label.config(text="")
+        self.label.update_idletasks()
 
 class UI:
     def __init__(self):
@@ -15,12 +27,25 @@ class UI:
         self.currentListSelection = 0
         self.log.info("Entering -> GUI -> Constructor()")
 
+
         #TOPLEVEL FRAMEWORK
         self.root = Tk()
         self.root.title("Drinkatron v%s"%constants.VERSION)
-        self.frame = Frame(self.root, width=800, height=600)
-        self.frame.pack()
-        
+        self.root.geometry("800x600")
+        self.frame = Frame(self.root, width=800, height=550)
+
+        self.status = StatusBar(self.root)
+        self.status.set("Initializing UI...")
+
+
+
+        #toolbar work
+        self.toolbar = Frame(self.frame)
+        self.exitButton = Button(self.toolbar,text="Add Drink", width = 9, command = self.createNewDrink)
+        self.exitButton.pack(side=LEFT, padx=2, pady=2)
+
+        self.toolbar.pack(side=BOTTOM)
+
         #MENUWORK
         self.menu = Menu(self.root)
         self.root.config(menu=self.menu)
@@ -32,11 +57,17 @@ class UI:
         self.fileMenu.add_command(label="Quit", command=self.menuQuit)
         self.log.info("Entering -> GUI -> mainLoop()")
 
+
+        #pack the frame first as both frame and status bar are children to root, this sets them up well.
+        self.frame.pack(fill=X)
+        self.status.pack(side=BOTTOM, fill=X)
+
+
         self.root.mainloop()
         self.log.info("Leaving  -> GUI -> Constructor()")
 
     def aboutMessageBox(self):
-        msgbox = tkMessageBox.showinfo("About", "Drinkatron is a personal bartender\n version: %s\nCopyright 2012"%constants.VERSION) 
+        msgbox = messagebox.showinfo("About", "Drinkatron is a personal bartender\n version: %s\nCopyright 2012"%constants.VERSION) 
         #fix for 3.2
     def menuQuit(self):
         self.log.info("Leaving  -> GUI -> mainLoop, quit was called")
@@ -44,14 +75,18 @@ class UI:
 
     def pourIt(self):
         self.log.info("Entering -> GUI ->  PourIt()")
+        if messagebox.askyesno("Drinkin' Time?", "Are you sure the cup is in place and this is the drink you want?"):
+            pass
         #drinkSelected = grab current selection from thing.
         #thread.start_new_thread(arduino.dispenseDrink, [drinkSelected.id,])
         self.log.info("Leaving  -> GUI -> Pourit()")
         pass
     
-        
+    def createNewDrink(self):
+        pass
 
 
 
 app = UI()
+
         
