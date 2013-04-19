cf815c53d2b8db1eca618aa0f23d34bf24487638
Gary Graham <garygrantgraham@gmail.com>
2013-04-19 18:11:09 -0400

    Worked on #30 Closed #27
    will now automatically increase dispense count when dispense method is
    called.
    also, started analytics work with functions to pull data over a range of
    dates based on drink dispense counts.

--- Python Work/dbinterface.py
+++ Python Work/dbinterface.py
@@ -1,9 +1,8 @@
-import re
 import sqlite3
 import logging
-import drinks
 import constants
-
+import datetime
+import time
 
 logging.basicConfig(file="runLog.txt", level=logging.INFO)
 log = logging.getLogger("Master")
@@ -62,35 +61,98 @@ class DB:
 
     def getOverdueCustomers(self):
         self.log.info("Entering -> getOverdueCustomers")
-        sql = "SELECT * FROM customers WHERE current_balance > 0 AND last_paid_date > today() - 30"  # TODO fix this for sqlite
+        # TODO fix this for sqlite
+        sql = '''SELECT *
+                 FROM customers
+                 WHERE current_balance > 0
+                 AND last_paid_date > today() - 30'''
         results = self.executeSql(sql)
+        return results
 
+    def getOverallDrinkDataBetweenDates(self, startDate=None, endDate=None):
+        # this checks the cases where the dates are not selected
+        # properly by the user, and puts in assumed data
+        # (i.e. today, a month ago, whateva whateva i do what i want)
+        if startDate is not None and endDate is None:
+            endDate = datetime.datetime.now()
+        elif startDate is None and endDate is None:
+            endDate = datetime.datetime.now()
+            startDate = endDate - datetime.timedelta(days=30)
+            print("Start date is: " + startDate)
+            print("End date is: " + endDate)
+        elif startDate is None and endDate is not None:
+            startDate = endDate - datetime.timedelta(days=30)
+
+        #if isinstance(endDate, time.struct_time):
+        #    endDate = endDate.strftime('%Y-%m-%d')
+        #    print("WOOP")
+        #if isinstance(startDate, time.struct_time):
+        #    startDate = startDate.strftime('%Y-%m-%d')
+        #    print("WOOP")
+
+        self.log.info("Start Date is: %s" % str(startDate))
+        self.log.info("End Date is: %s" % str(endDate))
+        args = (startDate, endDate)
+        sql = '''
+                SELECT drinks.drink_name , Count(*)
+                FROM dispenses, drinks
+                WHERE dispenses.drink_id = drinks.drink_id
+                AND dispenses.dispense_timestamp > ?
+                AND dispenses.dispense_timestamp < ?
+                group by drinks.drink_name
+              '''
+        resultSet = self.executeSql(sql, args)
+        return resultSet
+
+
+
+        def getAllTimeDrinkData():
+            sql = '''SELECT drink_name, dispense_count,
+                            positive_votes, negative_votes
+                     FROM drinks
+                  '''
+            results = self.executeSql(sql)
+
+
+
+    def dispenseOccured(self, drink_id):
+        self.log.info("Entering -> dispenseOccured")
+        args = (drink_id,)
+        sql = '''UPDATE drinks
+                 SET dispense_count = dispense_count + 1
+                 WHERE drink_id = ?'''
+        result1 = self.executeSql(sql, args)
+        sql = '''INSERT INTO dispenses (drink_id)
+                 VALUES (?)
+              '''
+        result2 = self.executeSql(sql, args)
+        return result1, result2
     #
     # MASTER LIST FUNCTIONMASTERLOL###
-    #
-    def executeSql(self, sql, values):
+
+    def executeSql(self, sql, args=None):
         self.log.info("Entering-> executeSql()")
         cursor = self.grab_cursor()
 
         # Shooting off SQL to DB and pulling returned list as tuples.
-        if values is None:
+        if args is None:
             cursor.execute(sql)
-        elif values is not None:
-            cursor.execute(sql, values)
+        elif args is not None:
+            cursor.execute(sql, args)
         else:
-            self.log.error("listDrinks -> Cannot resolve Values parameter")
-
+            self.log.error("listDrinks -> Cannot resolve args parameter")
+        self.conn.commit()
         resultSet = cursor.fetchall()
 
         # Checking integrity and returning
-        if resultSet == None:
+        if resultSet is None:
             self.log.error("No rows returned, empty DB? Wrong Table?")
         else:
             self.log.info("Grabbed resultSet list...")
-            for record in resultSet:
-                for column in record:
-                    if column == "'0'":
-                        column = 0
+            #for record in resultSet:
+            #    for column in record:
+            #        if column == "'0'":
+            #            column = 0
 
         self.log.info("Leaving -> executeSql")
 
@@ -108,16 +170,21 @@ class DB:
         self.log.info("Grabbed a cursor...")
         return cursor
 
-    def createNewDrink(self, ingredient1, amount1, ingredient2, amount2, ingredient3, amount3, ingredient4, amount4):
+    def createNewDrink(self, ingredient1, amount1, ingredient2, amount2,
+                       ingredient3, amount3, ingredient4, amount4):
         pass
 
     def updateDrink(self, drink):
         self.log.info("Entering -> updateDrink()")
-        sql = "UPDATE drinks SET popularity=%, dispenseCount=% WHERE drink_id=%" % (
-            drink.popularity, drink.dispenseCount, drink.id)
+        sql = '''UPDATE drinks
+                 SET popularity=%,
+                 dispenseCount=%
+                 WHERE drink_id=%''' % (drink.popularity,
+                                        drink.dispenseCount,
+                                        drink.id)
+
         cursor = self.grab_cursor()
         cursor.execute(sql)
-        cursor.commit()
         self.log.info("Leaving -> updateDrink()")
 
     def removeDrink(self, drink_id):
@@ -125,13 +192,10 @@ class DB:
         cursor = self.grab_cursor()
         sql = "DELETE * FROM drinks WHERE drink_id = ?"
         cursor.execute(sql, drink_id)
-        cursor.commit()
-
         sql = "SELECT * FROM drinks WHERE drink_id = ?"
         cursor.execute(sql, drink_id)
         results = cursor.fetchall()
-        cursor.commit()
-        if results == None or results == []:
+        if results is None or results == []:
             self.log.info("Leaving -> removeDrink")
             return True
         else:
