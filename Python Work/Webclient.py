080c912ad5516d0afd971a9c27708cbcacd590e7
Gary Graham <garygrantgraham@gmail.com>
2013-04-22 16:26:23 -0400

    Split up the known and custom drinks closed #24
    both now throw calls to pourDrink which cleans the list before dumping
    it off to the arduino.

--- Python Work/Webclient.py
+++ Python Work/Webclient.py
@@ -1,5 +1,4 @@
 import bottle
-import bottle_sqlite
 import dbinterface
 import drinks
 import logging
@@ -9,13 +8,16 @@ import arduinocomm
 
 arduino = arduinocomm.Connection()
 
+
 @bottle.route('/')
 def index():
     return bottle.template('index')
 
+
 @bottle.route('/getDrinks')
 def getDrinks():
     return bottle.template('getDrinks', drinkDictList=drinkDictList)
+
 
 @bottle.route('/getDrink/:name')
 def getDrink(name):
@@ -25,36 +27,57 @@ def getDrink(name):
             print(drink)
             return drink
 
-# todo write code to do a lookup through getDrink/:name and then dispense
-# based upon default ingredient proportions
-@bottle.route('/dispense/:name')
+
+@bottle.route('/dispense/known/:name')
 def dispense(name):
     print(name)
-    cleanList = []
+    dirtyList = []
     for drink in drinkDictList:
         if drink['name'] == name:
-            cleanList.append(drink[constants.INGREDIENTLIST[0]])
-            cleanList.append(drink[constants.INGREDIENTLIST[1]])
-            cleanList.append(drink[constants.INGREDIENTLIST[2]])
-            cleanList.append(drink[constants.INGREDIENTLIST[3]])
-            cleanList.append(drink[constants.INGREDIENTLIST[4]])
-            cleanList.append(drink[constants.INGREDIENTLIST[5]])
-            cleanList.append(drink[constants.INGREDIENTLIST[6]])
-            cleanList.append(drink[constants.INGREDIENTLIST[7]])
-            cleanList.append(drink[constants.INGREDIENTLIST[8]])
-            cleanList.append(drink[constants.INGREDIENTLIST[9]])
-            cleanList.append(drink[constants.INGREDIENTLIST[10]])
-            cleanList.append(drink[constants.INGREDIENTLIST[11]])
-            print(cleanList)
+            dirtyList.append(drink[constants.INGREDIENTLIST[0]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[1]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[2]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[3]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[4]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[5]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[6]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[7]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[8]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[9]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[10]])
+            dirtyList.append(drink[constants.INGREDIENTLIST[11]])
+            print(dirtyList)
             db.dispenseOccured(drink['drinkID'])
-            arduino.sendDrink(cleanList)
-            return json.dumps(drink, indent=4)
-
     return "Drink Could not be found!"
+
+
+@bottle.route('/dispense/custom/:adhocList')
+def dispense_custom(adhocList):
+    adhocList = adhocList.split('_')
+    dirtyList = []
+    for item in adhocList:
+        dirtyList.append(int(item))
+    print(dirtyList)
+    pourDrink(dirtyList)
+
+
+def pourDrink(dirtyList):
+    #making the drink proportional given a 100% is a full drink.
+    drinkSize = 0
+    cleanedList = []
+    for item in dirtyList:
+        drinkSize += item
+    if drinkSize != 100:
+        for i in range(len(dirtyList)):
+            cleanedList.append(round(dirtyList[i] / float(drinkSize) * 100))
+
+    print(cleanedList)
+    arduino.sendDrink(cleanedList)
 
 @bottle.route('/Analytics')
 def Analytics():
-    return bottle.template('Analytics', data = drinkData)
+    drinkData = []
+    return bottle.template('Analytics', data=drinkData)
 
 
 # todo Write code to split up the string into sections indicating proportions.
@@ -68,8 +91,7 @@ if __name__ == '__main__':
     db = dbinterface.DB()
     drinkList = db.listDrinksByName()
 
-
-        # THIS IS CRUCIAL. this code initializes ALL drinks into an array,
+    # THIS IS CRUCIAL. this code initializes ALL drinks into an array,
     # all at once. DO NOT FUCK WITH THIS CODE.
     drinkObjArray = []
     ingredientDict = {}
@@ -85,7 +107,6 @@ if __name__ == '__main__':
         drinkDictList.append(tempDrink.convertToDict())
         db.log.info(drinkObjArray[currentDrink].drinkName)
         #
-
 
     for item in drinkDictList:
         print(item)
